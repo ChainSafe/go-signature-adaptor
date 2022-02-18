c2e7f7dd454c02312e226a1aa0dda93dfc5f776a
noot <elizabethjbinks@gmail.com>
2022-02-18 02:07:38 -0500

    implement adaptor sign/verify

--- secp256k1/adaptor_test.go
+++ secp256k1/adaptor_test.go
@@ -6,15 +6,14 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestECDSA_SignAndVerify(t *testing.T) {
+func TestAdaptor_SignAndVerify(t *testing.T) {
 	kp := GenerateKeypair()
 
 	msg := [32]byte{1, 2, 3}
-	sig, err := kp.Sign(msg[:])
+	sig, err := kp.AdaptorSign(msg[:])
 	require.NoError(t, err)
 
-	msg = [32]byte{1, 2, 3}
-	ok, err := kp.Public().Verify(msg[:], sig)
+	ok, err := kp.Public().VerifyAdaptor(msg[:], sig.AdaptorWithSecret.adaptor)
 	require.NoError(t, err)
 	require.True(t, ok)
 }
